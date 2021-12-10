4504dade0ab99520788c488d9605d7b687946605
Kseniya Kharchenko <k.burnashova@lenvendo.ru>
2021-12-10 15:22:25 +0300

    new components

--- vue/verbal-counting-app/src/components/GamePage.vue
+++ vue/verbal-counting-app/src/components/GamePage.vue
@@ -1,14 +1,62 @@
 <template>
   <p>GamePage</p>
+  <div class="gamePageHeader">
+    <button @click="stopGame">Отмена</button>
+    <Timer :handle-show-modal="handleShowModal"/>
+  </div>
+  <Keyboard />
+
+  <div v-if="showModal">
+    <Modal :handle-close-modal="handleCloseModal"/>
+  </div>
 </template>
 
 <script>
+import Timer from '@/components/Timer';
+import {useRouter} from 'vue-router';
+import Keyboard from '@/components/Keyboard';
+import {computed, ref} from 'vue';
+import Modal from '@/components/Modal';
+
 export default {
   name: 'GamePage',
-  id: 'GamePage'
+
+  components: {
+    Timer,
+    Keyboard,
+    Modal
+  },
+
+  setup() {
+    const router = useRouter();
+
+    let showModal = ref(false);
+
+    const stopGame = () => {
+      router.push('/')
+    };
+
+    const handleShowModal = () => {
+      showModal.value = true;
+    };
+
+    const handleCloseModal = () => {
+      showModal.value = false;
+    };
+
+    return {
+      stopGame,
+      showModal,
+      handleShowModal,
+      handleCloseModal
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.gamePageHeader {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
